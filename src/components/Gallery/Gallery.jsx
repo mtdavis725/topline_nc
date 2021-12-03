62a91bde43eb1f2ef718af4be268d2ed9b0e6e72
Maisson Davis <68596011+mtdavis725@users.noreply.github.com>
2021-12-03 15:40:06 -0500

    Install node-sass and added components folder with subfolders About and Gallery

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+/* import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'; */
+import Gallery from './components/Gallery/Gallery';
+import './App.scss';
+
+function App() {
+  return (
+    <Gallery />
+  );
+}
+
+export default App;
