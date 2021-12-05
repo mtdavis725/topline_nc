c9a75eda9c693cb15577001ed78e283fc11de70d
Maisson Davis <68596011+mtdavis725@users.noreply.github.com>
2021-12-05 08:44:26 -0500

    Manual import and format lessons and contact pages into components

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Lessons from './components/Lessons/Lessons';
 import Boarding from './components/Boarding/Boarding';
 import Gallery from './components/Gallery/Gallery';
+import Contact from './components/Contact/Contact';
 
 import './App.scss';
 
@@ -15,8 +17,10 @@ function App() {
       <Header />
       <Home />
       <About />
+      <Lessons />
       <Boarding />
       <Gallery />
+      <Contact />
       <Footer />
     </div>
     
