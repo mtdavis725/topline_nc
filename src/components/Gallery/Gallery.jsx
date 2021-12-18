4f40e8fc11abb79eb25afd99b52855a2907ae704
Maisson Davis <davismaisson@gmail.com>
2021-12-18 14:06:27 -0500

    Create galleryImages variable, add css grid to gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,6 +1,10 @@
 import images from '../../images' /*Array of images from images folder*/
 import { Link } from 'react-router-dom'
 
+let galleryImages = images.gallery.map((item, index) => {
+    return <img src={images.gallery[index]} alt={item}  />
+    })
+
 function Gallery() {
     return (
         <section>
@@ -19,68 +23,9 @@ function Gallery() {
                     </div>
                 </div>
 
-                <div className="container-fluid">
-                    <div className="row mx-auto">
-                        <div className="col">
-                            <img src={images.gallery[0]} alt="gallery" className="img-fluid p-1 p-1" />
-                            <img src={images.gallery[1]} alt="gallery" className="img-fluid p-1" />
-                            <img src={images.gallery[2]} alt="gallery" className="img-fluid p-1" />
-                            <img src={images.gallery[3]} alt="gallery" className="img-fluid p-1" />
-                            <img src={images.gallery[4]} alt="gallery" className="img-fluid p-1" />
-                            <img src={images.gallery[5]} alt="gallery" className="img-fluid p-1" /> 
-                        </div>
-
-                        <div className="col">
-                            <img src={images.gallery[6]} alt="gallery" className="img-fluid p-1" />
-                            <img src={images.gallery[7]} alt="gallery" className="img-fluid p-1" />
-                            <img src={images.gallery[8]} alt="gallery" className="img-fluid p-1" />
-                            <img src={images.gallery[9]} alt="gallery" className="img-fluid p-1" />
-                            <img src={images.gallery[10]} alt="gallery" className="img-fluid p-1" />
-                            <img src={images.gallery[11]} alt="gallery" className="img-fluid p-1" />
-                        </div>
-
-                        <div className="col">
-                            <img src={images.gallery[12]} alt="gallery" className="img-fluid p-1" />
-                            <img src={images.gallery[13]} alt="gallery" className="img-fluid p-1" />
-                            <img src={images.gallery[14]} alt="gallery" className="img-fluid p-1" />
-                        
-                        
-                        </div>     
-                    </div>   
-                </div>
-
-                
-
-                <div className="gallery">
-                
-
-
-                    <div className="col">
-                        <a href="./images/gallery-Laura-with-kids-ribbons.jpg" data-lightbox="gallery"><img alt="" src="./images/gallery-Laura-with-kids-ribbons.jpg" className="img-fluid" /></a>
-                        <a href="./images/gallery-braids-w960-o.jpg" data-lightbox="gallery"><img alt="" src="./images/gallery-braids-w960-o.jpg" className="img-fluid" /></a>
-
-                        <a href="../images/gallery-lesson.jpg" data-lightbox="gallery"><img alt="" src="../images/gallery-lesson.jpg" className="img-fluid" /></a>
-                        <a href="../images/gallery-beau-w795-o.jpg" data-lightbox="gallery"><img alt="" src="../images/gallery-beau-w795-o.jpg" className="img-fluid" /></a>
-                        <a href="../images/gallery-sammi.jpg" data-lightbox="gallery"><img alt="" src="../images/gallery-sammi.jpg" className="img-fluid" /></a>
-                        <a href="../images/gallery-laura-kids-indoor.jpg" data-lightbox="gallery"><img alt="" src="../images/gallery-laura-kids-indoor.jpg" className="img-fluid" /></a>
-                    </div>
-                    <div className="col">
-                        <a href="../images/gallery-sammi-cotton.jpg" data-lightbox="gallery"><img alt="" src="../images/gallery-sammi-cotton.jpg" className="img-fluid" /></a>
-                        <a href="../images/galleryvero-sillhouette.JPG" data-lightbox="gallery"><img alt="" src="../images/galleryvero-sillhouette.JPG" className="img-fluid" /></a>
-                        <a href="../images/gallery-peekaboo.JPG" data-lightbox="gallery"><img alt="" src="../images/gallery-peekaboo.JPG" className="img-fluid" /></a>
-                        <a href="../images/gallery-outdoor-arena.jpg" data-lightbox="gallery"><img alt="" src="../images/gallery-outdoor-arena.jpg" className="img-fluid" /></a>
-                        <a href="../images/gallery-mom_and_laddie.jpg" data-lightbox="gallery"><img alt="" src="../images/gallery-mom_and_laddie.jpg" className="img-fluid" /></a>
-                        <a href="../images/gallery-bw-laura-taylor.jpg" data-lightbox="gallery"><img alt="" src="../images/gallery-bw-laura-taylor.jpg" className="img-fluid" /></a>
-                    </div>
-                    <div className="col">
-                        <a href="../images/gallery-trail.jpg" data-lightbox="gallery"><img alt="" src="../images/gallery-trail.jpg" className="img-fluid" /></a>
-                        <a href="../images/gallery-jumping.jpg" data-lightbox="gallery"><img alt="" src="../images/gallery-jumping.jpg" className="img-fluid" /></a>
-                        <a href="../images/gallery-horse-anatomy-lesson.jpg" data-lightbox="gallery"><img alt="" src="../images/gallery-horse-anatomy-lesson.jpg" className="img-fluid" /></a>
-                        <a href="../images/gallery-kisses_for_Ali.jpg" data-lightbox="gallery"><img alt="" src="../images/gallery-kisses_for_Ali.jpg" className="img-fluid" /></a>
-                        <a href="../images/gallery-gabby.jpg" data-lightbox="gallery"><img alt="" src="../images/gallery-gabby.jpg" className="img-fluid" /></a>
-                        <a href="../images/gallery-Laura-riding.jpg" data-lightbox="gallery"><img alt="" src="../images/gallery-Laura-riding.jpg" className="img-fluid" /></a>
-                    </div>
-                </div>
+                <div className="gallery-container">
+                    {galleryImages}                
+                </div>  
             </main>
         </section>
 
