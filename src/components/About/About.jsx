491f593baaa92e41bf637ab296de72750e15da35
Maisson Davis <davismaisson@gmail.com>
2021-12-18 19:28:57 -0500

    Adding css grid to about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,10 +17,56 @@ function About() {
         <hr className="bg-light w-75 mx-auto" />
 
 
-        <div className="container-fluid padding">
-            <div className="row align-items-start justify-content-center mx-auto ">
+
+        <div className="about-container padding">
+            
                 
-                <div className="col-sm-12 col-md-8 col-lg-9 pb-0">
+                <div>
+                    <h2 className="content-header pb-1">Laura Rose Heymann</h2>
+                    <p>Laura's devotion to all things horses began at the age of six in Charlottesville, VA, when her mother first enrolled her in riding lessons. From there, Laura became increasingly absorbed in the horseback riding community as a hunter/jumper, eventer, and foxhunter, under the instruction of Page Turner. </p>
+                    <p>With 11 years of intense training from Page, Page has provided the foundation of Laura's training methods. Through Page, Laura was first introduced to Natural Horsemanship by Louis Wood. </p>
+                </div>
+
+                <div>
+                    <img src={images.about[0]} alt="VA countryside" className="img-fluid" />
+                </div>
+
+
+                <div>
+                    <img src={images.about[1]} alt="Laura Heymann" className="img-fluid" />
+                </div>
+
+                <div>
+                    <p>Natural Horsemanship drastically altered Laura's perception of working with horses and provided her with a new trajectory towards not just riding, but the actual training and teaching of the horse. Thanks to Page and Louis (and Laura's supportive parents :) Laura had the opportunity to participate in dozens of clinics on different horses of various skill levels. This instruction continued until Laura was accepted to Elon University in Elon, NC.</p>
+                    <p>Once at Elon, Laura focused entirely on Dressage and competed on the Intercollegiate team, as well as in USDF rated shows through 1st level with scores in the 70's. Immediately upon graduating in 2008 with a B.A in Sociology, Laura settled on starting a business and opened Topline Training in August 2008.</p>
+                </div>
+
+
+                <div>
+                    <p>
+                        After freelancing ​for 8 years, Laura had the opportunity to settle into one location which is
+                        now called Green Level Equestrian in Apex. Laura currently has a very busy M-F youth and adult
+                        lesson program​ with many events offered on Saturdays for lesson clients to participate in or
+                        attend.
+                    </p>
+                    <p>
+                        The main goal of the barn is to provide a kind, open, judgement-free zone for horses and riders
+                        to learn and relax while maintaining top quality horse care.
+                        ​</p>
+                </div>
+
+                <div>
+                    <img src={images.about[2]} alt="GLE" className="img-fluid" />
+                </div>
+
+            </div>
+        
+
+
+        <div className="container-fluid padding">
+            <div className="row align-items-start justify-content-center mx-auto padding">
+                
+                <div className="col-sm-12 col-md-8 col-lg-9">
                     <h2 className="content-header pb-1">Laura Rose Heymann
                     </h2>
                     <p>Laura's devotion to all things horses began at the age of six in Charlottesville, VA, when her
@@ -41,9 +87,9 @@ function About() {
 
 
                 <div className="col-6 col-md-4 col-lg-3">
-                    <img src={images.about[1]} alt="Laura Heymann" className="img-fluid border rounded" />
+                    <img src={images.about[1]} alt="Laura Heymann" className="img-fluid" />
                 </div>
-                <div className="col-sm-12 col-md-8 col-lg-9 pb-0">
+                <div className="col-sm-12 col-md-8 col-lg-9">
                     <p>
                         Natural Horsemanship drastically altered Laura's perception of working with horses and provided
                         her with a new trajectory towards not just riding, but the actual training and teaching of the
@@ -73,7 +119,7 @@ function About() {
                         ​</p>
                 </div>
                 <div className="col-sm-10 col-md-6 col-lg-5">
-                    <img src={images.about[2]} alt="GLE" className="img-fluid border rounded p-0" />
+                    <img src={images.about[2]} alt="GLE" className="img-fluid" />
                 </div>
 
             </div>
@@ -82,25 +128,19 @@ function About() {
 
        
         <div className="container-fluid padding">
-            <div className="row mx-auto">
+            <div className="row mx-auto padding">
                 <div className="col-12">
                     <h1 className="text-center page-header">Training Philosophy</h1>
                     <hr className="bg-light w-75 mx-auto" />
-                    <p>Laura has a unique approach which concentrates on the partnership between horse and rider. Her
-                        practice allows the horse to become a more willing participant to help achieve the rider's
-                        goals.
-                    </p>
-                    <p>
-                        Laura’s training is premised on the fact that no one horse has the same background as any other.
-                        From their status within the herd to previous injuries or accidents, each horse enters the
-                        riding arena with a different set of experiences. Therefore, using the same set of exercises to
-                        train every horse does not produce a completely successful outcome, nor does it make sense.</p>
+                    <p>Laura has a unique approach which concentrates on the partnership between horse and rider. Her practice allows the horse to become a more willing participant to help achieve the rider's
+                    goals.</p>
+                    <p>Laura’s training is premised on the fact that no one horse has the same background as any other. From their status within the herd to previous injuries or accidents, each horse enters the riding arena with a different set of experiences. Therefore, using the same set of exercises to train every horse does not produce a completely successful outcome, nor does it make sense.</p>
                 </div>
             </div>
 
 
 
-            <div className="row mx-auto justify-content-center">
+            <div className="row mx-auto justify-content-center padding">
                 <div className="col-sm-10 text-center">
 
                     <blockquote className="blockquote"><i className="fa fa-quote-left"> </i> The key to having an
