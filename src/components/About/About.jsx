3a4d244cafd9206592b5d24841d96ac424d208ac
Maisson Davis <davismaisson@gmail.com>
2021-12-22 08:07:38 -0500

    Editing about component layout

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,32 +17,29 @@ function About() {
         <hr className="bg-light w-75 mx-auto" />
 
 
-
-        <div className="about-container padding">
+        
+        <div className="about-container padding mt-3">
             
                 
-                <div>
+                <div className="item1">
                     <h2 className="content-header pb-1">Laura Rose Heymann</h2>
                     <p>Laura's devotion to all things horses began at the age of six in Charlottesville, VA, when her mother first enrolled her in riding lessons. From there, Laura became increasingly absorbed in the horseback riding community as a hunter/jumper, eventer, and foxhunter, under the instruction of Page Turner. </p>
                     <p>With 11 years of intense training from Page, Page has provided the foundation of Laura's training methods. Through Page, Laura was first introduced to Natural Horsemanship by Louis Wood. </p>
+                    <p>Natural Horsemanship drastically altered Laura's perception of working with horses and provided her with a new trajectory towards not just riding, but the actual training and teaching of the horse. Thanks to Page and Louis (and Laura's supportive parents :) Laura had the opportunity to participate in dozens of clinics on different horses of various skill levels.</p>
                 </div>
 
-                <div>
+                <div className="item2">
                     <img src={images.about[0]} alt="VA countryside" className="img-fluid" />
                 </div>
 
 
-                <div>
+                <div className="item3">
                     <img src={images.about[1]} alt="Laura Heymann" className="img-fluid" />
                 </div>
 
-                <div>
-                    <p>Natural Horsemanship drastically altered Laura's perception of working with horses and provided her with a new trajectory towards not just riding, but the actual training and teaching of the horse. Thanks to Page and Louis (and Laura's supportive parents :) Laura had the opportunity to participate in dozens of clinics on different horses of various skill levels. This instruction continued until Laura was accepted to Elon University in Elon, NC.</p>
-                    <p>Once at Elon, Laura focused entirely on Dressage and competed on the Intercollegiate team, as well as in USDF rated shows through 1st level with scores in the 70's. Immediately upon graduating in 2008 with a B.A in Sociology, Laura settled on starting a business and opened Topline Training in August 2008.</p>
-                </div>
-
-
-                <div>
+                <div className="item4">
+                    
+                    <p>This instruction continued until Laura was accepted to Elon University in Elon, NC. Once at Elon, Laura focused entirely on Dressage and competed on the Intercollegiate team, as well as in USDF rated shows through 1st level with scores in the 70's. Immediately upon graduating in 2008 with a B.A in Sociology, Laura settled on starting a business and opened Topline Training in August 2008.</p>
                     <p>
                         After freelancing ​for 8 years, Laura had the opportunity to settle into one location which is
                         now called Green Level Equestrian in Apex. Laura currently has a very busy M-F youth and adult
@@ -55,75 +52,11 @@ function About() {
                         ​</p>
                 </div>
 
-                <div>
+                <div className="item6">
                     <img src={images.about[2]} alt="GLE" className="img-fluid" />
                 </div>
 
             </div>
-        
-
-
-        <div className="container-fluid padding">
-            <div className="row align-items-start justify-content-center mx-auto padding">
-                
-                <div className="col-sm-12 col-md-8 col-lg-9">
-                    <h2 className="content-header pb-1">Laura Rose Heymann
-                    </h2>
-                    <p>Laura's devotion to all things horses began at the age of six in Charlottesville, VA, when her
-                        mother first enrolled her in riding lessons. From there, Laura became increasingly absorbed in
-                        the horseback riding community as a hunter/jumper, eventer, and foxhunter, under the instruction
-                        of Page Turner. </p>
-
-                    <p>With 11 years of intense training from Page, Page has provided the foundation of Laura's training
-                        methods. Through Page, Laura was first introduced to Natural Horsemanship by Louis Wood.
-                    </p>
-
-
-                </div>
-                <div className="col-6 col-md-4 col-lg-3">
-                    <img src={images.about[0]} alt="VA countryside" className="img-fluid" />
-
-                </div>
-
-
-                <div className="col-6 col-md-4 col-lg-3">
-                    <img src={images.about[1]} alt="Laura Heymann" className="img-fluid" />
-                </div>
-                <div className="col-sm-12 col-md-8 col-lg-9">
-                    <p>
-                        Natural Horsemanship drastically altered Laura's perception of working with horses and provided
-                        her with a new trajectory towards not just riding, but the actual training and teaching of the
-                        horse. Thanks to Page and Louis (and Laura's supportive parents :) Laura had the opportunity to
-                        participate in dozens of clinics on different horses of various skill levels. This instruction
-                        continued until Laura was accepted to Elon University in Elon, NC.
-                    </p>
-                    <p>
-                        Once at Elon, Laura focused entirely on Dressage and competed on the Intercollegiate team, as
-                        well as in USDF rated shows through 1st level with scores in the 70's. Immediately upon
-                        graduating in 2008 with a B.A in Sociology, Laura settled on starting a business and opened
-                        Topline Training in August 2008.
-                    </p>
-                </div>
-
-
-                <div className="col-sm-12 col-md-6 col-lg-7">
-                    <p>
-                        After freelancing ​for 8 years, Laura had the opportunity to settle into one location which is
-                        now called Green Level Equestrian in Apex. Laura currently has a very busy M-F youth and adult
-                        lesson program​ with many events offered on Saturdays for lesson clients to participate in or
-                        attend.
-                    </p>
-                    <p>
-                        The main goal of the barn is to provide a kind, open, judgement-free zone for horses and riders
-                        to learn and relax while maintaining top quality horse care.
-                        ​</p>
-                </div>
-                <div className="col-sm-10 col-md-6 col-lg-5">
-                    <img src={images.about[2]} alt="GLE" className="img-fluid" />
-                </div>
-
-            </div>
-        </div>
 
 
        
