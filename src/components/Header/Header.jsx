62aa3be620545d9505a346a88e90353720393ac7
Maisson Davis <davismaisson@gmail.com>
2022-01-15 10:39:12 -0500

    Working on mobile navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
 import images from '../../images'
 import { NavLink, useLocation } from 'react-router-dom'
+import { useState } from 'react';
 
 
 function Header() {
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+    const handleNavCollapse = () => {
+        setIsNavCollapsed(!isNavCollapsed);
+    }
     const location = useLocation();
     const pathname = location.pathname;
     const currentPath = (pathname) => {
@@ -25,42 +30,42 @@ function Header() {
                     <a href="https://www.facebook.com/greenlevelequestrian" target="./blank" className="mobile-hidden">
                         <i className="fa fa-facebook-square social-link"></i>
                     </a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        {currentPath(pathname)}
-                        <i class="fa fa-arrow-down d-block"></i>
+                    <button className="navbar-toggler nav-link" type="button" data-toggle="collapse"
+                        data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" >
+                        {currentPath(pathname) + " "}
+                        <i class="fa fa-angle-down"></i>
                     </button>
-                    {/*className="collapse navbar-collapse"*/}
-                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                    
+                    <div className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`} id="navbarResponsive">
                         <ul className="navbar-nav mx-auto">
-                            <li className="nav-item">
+                            
+                            <li className="nav-item"  onClick={handleNavCollapse}>
                                 <NavLink to="./" className="nav-link">
                                     Home
                                     <span class="sr-only">(current)</span>
                                 </NavLink>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item"  onClick={handleNavCollapse}>
                                 <NavLink to="./about" className="nav-link">
                                     About
                                 </NavLink>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item"  onClick={handleNavCollapse}>
                                 <NavLink to="./lessons" className="nav-link">
                                     Lessons
                                 </NavLink>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item"  onClick={handleNavCollapse}>
                                 <NavLink to="./boarding" className="nav-link">
                                     Boarding
                                 </NavLink>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item"  onClick={handleNavCollapse}>
                                 <NavLink to="./gallery" className="nav-link">
                                     Gallery
                                 </NavLink>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item"  onClick={handleNavCollapse}>
                                 <NavLink to="./contact" className="nav-link">
                                     Contact
                                 </NavLink>
